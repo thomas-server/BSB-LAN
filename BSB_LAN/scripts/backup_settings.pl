317566ba51614a31b1eefad2c7d07d4d9314f62d
Thomas LESIEUR <thomas.lesieur@outlook.com>
2023-11-05 19:37:29 +0100

    change export param script

--- BSB_LAN/scripts/backup_settings.pl
+++ BSB_LAN/scripts/backup_settings.pl
@@ -6,7 +6,11 @@
 # All you need to do is adjust the URL of BSB-LAN in the line below (including
 # any potential credentials/passcode). Do NOT end the URL with a slash!
 
-my $URL = "http://192.168.1.50/4444";
+# Prompt for IP and port
+print "Enter the IP address and port of the BSB-LAN device (e.g., '192.168.1.90/80'): ";
+my $ip_port = <STDIN>;
+chomp($ip_port);
+my $URL = "http://$ip_port";
 
 my $from = $ARGV[0];
 my $to = $ARGV[1];
@@ -17,12 +21,31 @@ if ($to le " " ) {
   $to = 9999;
 }
 
+
+# Prompt for credentials
+print "Username: ";
+my $username = <STDIN>;
+chomp($username);
+
+print "Password: ";
+system "stty -echo";  # Disable echo for password input
+my $password = <STDIN>;
+chomp($password);
+system "stty echo";  # Re-enable echo
+print "\n";  # Print a newline since echo was disabled when the user pressed enter
+
+
 print "Creating backup for parameters $from to $to, this may take several minutes...\n";
 my $date = `date "+%Y%m%d"`;
 chomp ($date);
 
-open (OUT, ">backup_settings-$from-$to-$date.csv");
-open (WGET, "wget -q -O - $URL/$from-$to|");
+open(my $OUT, ">", "backup_settings-$from-$to-$date.csv") or die "Cannot open file: $!";
+
+# Construct and execute the wget command while printing it
+my $wget_cmd = "wget --http-user='$username' --http-password='$password' -q -O - $URL/$from-$to";
+print "Executing wget command: $wget_cmd\n";
+open(my $WGET, "-|", $wget_cmd) or die "Cannot execute wget: $!";
+
 while (my $line=<WGET>) {
   if ($line =~ /^\s*(\d+)\s+.*: (.*)/) {
     my $param_no = $1;
@@ -34,3 +57,5 @@ while (my $line=<WGET>) {
     print OUT "$param_no;$param_val\n";
   }
 }
+
+print "Backup completed.\n";
